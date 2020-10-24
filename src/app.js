0344b944f00934d4eb4f7c114504b7717fb58a69
Veer-Banerjee <67536722+Veer-Banerjee@users.noreply.github.com>
2020-10-24 18:54:01 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -47,18 +47,42 @@ function renderPatty() {
 
 function renderCheese() {
   //Trial 1 - Change the visibility of cheese based on state by manipulating the DOM
+  let cheese = document.querySelector("#cheese")
+  if (state.Cheese)
+    cheese.style.display = "inherit"
+  else
+    cheese.style.display = "none"
 }
 
 function renderTomatoes() {
   //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
+  let tomatoes = document.querySelector("#tomato")
+  if (state.Tomatoes) {
+    tomatoes.style.display = "inherit"
+  } else {
+    tomatoes.style.display = "none"
+  }
+
 }
 
 function renderOnions() {
   //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
+  let onions = document.querySelector("#onion")
+  if (state.Onions) {
+    onions.style.display = "inherit"
+  } else {
+    onions.style.display = "none"
+  }
 }
 
 function renderLettuce() {
   //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
+  let lettuce = document.querySelector("#lettuce")
+  if (state.Lettuce) {
+    lettuce.style.display = "inherit"
+  } else {
+    lettuce.style.display = "none"
+  }
 }
 
 document.querySelector(".btn-patty").onclick = function () {
@@ -66,6 +90,25 @@ document.querySelector(".btn-patty").onclick = function () {
   renderAll();
 };
 
+document.querySelector(".btn-cheese").onclick = function () {
+  state.Cheese = !state.Cheese;
+  renderAll();
+};
+
+document.querySelector(".btn-tomatoes").onclick = function () {
+  state.Tomatoes = !state.Tomatoes;
+  renderAll();
+};
+
+document.querySelector(".btn-onions").onclick = function () {
+  state.Onions = !state.Onions;
+  renderAll();
+};
+
+document.querySelector(".btn-lettuce").onclick = function () {
+  state.Lettuce = !state.Lettuce;
+  renderAll();
+};
 // Trial 2 - Setup event listener for the cheese button
 
 
@@ -87,3 +130,38 @@ document.querySelector(".btn-patty").onclick = function () {
 //Judgement 1
 //In the p element having price-details as the class, display the calculated
 //price based on ingredients
+
+function renderButtons() {
+  renderPatty()
+  renderCheese()
+  renderOnions()
+  renderTomatoes()
+  renderLettuce()
+}
+
+function renderIngredientsBoard() {
+  var items = document.getElementsByClassName("items")
+  if (state.Patty)
+    items[0].style.display = "inherit"
+  else
+    items[0].style.display = "none"
+  if (state.Cheese)
+    items[1].style.display = "inherit"
+  else
+    items[1].style.display = "none"
+
+  if (state.Tomatoes)
+    items[2].style.display = "inherit"
+  else
+    items[2].style.display = "none"
+
+  if (state.Onions)
+    items[3].style.display = "inherit"
+  else
+    items[3].style.display = "none"
+
+  if (state.Lettuce)
+    items[4].style.display = "inherit"
+  else
+    items[4].style.display = "none"
+}
